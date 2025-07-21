7a9e84365d4aaf5447bda529835cfec4f5d7ba84
hoangnguyenn26 <nguyencntt08@gmail.com>
2025-07-21 22:44:30 +0700

    fix(seo): update og:image to demo.png for social sharing thumbnail

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Hoàng Nguyên is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://hoangnguyenn.netlify.app', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/demo.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
   plugins: [
